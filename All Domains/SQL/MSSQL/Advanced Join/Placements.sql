45baa2620855e2b1a71610a08d9a088e5254412a
Shiv Thakur <sthakur@echo.quinstreet.net>
2016-11-22 11:01:01 -0800

    Commited solutions for three Hackerrank SQL problems under Advanced Join section

---
+++ All Domains/SQL/MSSQL/Advanced Join/Placements.sql
@@ -0,0 +1,6 @@
+/*
+
+You are given three tables: Students, Friends and Packages. Students contains two columns: ID and Name. Friends contains two columns: ID and Friend_ID (ID of the ONLY best friend). 
+Packages contains two columns: ID and Salary (offered salary in $ thousands per month). Write a query to output the names of those students whose best friends got offered a higher salary than them. 
+Names must be ordered by the salary amount offered to the best friends. It is guaranteed that no two students got same salary offer.
+*/
