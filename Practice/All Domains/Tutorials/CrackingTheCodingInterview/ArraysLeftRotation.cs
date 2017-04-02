8f7dfb69804539b36f0220262598b5ec10aca15c
Shiv Kr Thakur <shiv.kr.thakur@gmail.com>
2017-04-02 13:31:04 -0700

    HackerRank Tutorials Cracking the coding Interview problem Arrays: Left Rotation C# solution

---
+++ Practice/All Domains/Tutorials/CrackingTheCodingInterview/ArraysLeftRotation.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution {
+
+    static void Main(String[] args) {
+        string[] tokens_n = Console.ReadLine().Split(' ');
+        int n = Convert.ToInt32(tokens_n[0]);
+        int k = Convert.ToInt32(tokens_n[1]);
+        string[] a_temp = Console.ReadLine().Split(' ');
+        int[] a = Array.ConvertAll(a_temp,Int32.Parse);
+        
+        for(int x = 0; x < n; x++)
+        {
+            int index = ((n+k+x) % n);
+            Console.Write("{0} ", a[index]);
+        }
+    }
+}
