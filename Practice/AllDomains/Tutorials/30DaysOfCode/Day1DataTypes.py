681136ab1a322b1985a2756be31bca1d3c000bf2
shiv.kr.thakur <shiv.kr.thakur@hotmail.com>
2017-04-15 21:07:16 -0700

    Added Python solution for the Hacker Rank Practice Data Types problem.

---
+++ Practice/AllDomains/Tutorials/30DaysOfCode/Day1DataTypes.py
@@ -0,0 +1,15 @@
+/*	author:@shivkrthakur  */
+# Declare second integer, double, and String variables.
+input1 = input()
+input2 = input()
+input3 = input()
+# Read and save an integer, double, and String to your variables.
+
+# Print the sum of both integer variables on a new line.
+print(int(input1) + i) 
+print(float(input2) + d)
+print(s + input3)
+# Print the sum of the double variables on a new line.
+
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
