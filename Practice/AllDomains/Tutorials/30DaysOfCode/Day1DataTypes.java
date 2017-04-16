0951646c78d810659aea43312dc637907a9a37a4
shiv.kr.thakur <shiv.kr.thakur@hotmail.com>
2017-04-15 21:06:37 -0700

    Added Java solution for the Hacker Rank Practice Data Types problem.

---
+++ Practice/AllDomains/Tutorials/30DaysOfCode/Day1DataTypes.java
@@ -0,0 +1,15 @@
+/*	author:@shivkrthakur  */
+/* Declare second integer, double, and String variables. */
+int input1 = scan.nextInt();
+double input2 = scan.nextDouble();
+scan.nextLine();
+String input3 = scan.nextLine();
+/* Read and save an integer, double, and String to your variables.*/
+System.out.println(input1 + i);
+/* Print the sum of both integer variables on a new line. */
+
+/* Print the sum of the double variables on a new line. */
+System.out.println(input2 + d);
+/* Concatenate and print the String variables on a new line; 
+	the 's' variable above should be printed first. */
+System.out.println(s + input3);
