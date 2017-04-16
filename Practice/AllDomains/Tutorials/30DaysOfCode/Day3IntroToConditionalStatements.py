d7aea0ee5bba9a0b453a3de02dbbf812b9e9c5a7
shiv.kr.thakur <shiv.kr.thakur@hotmail.com>
2017-04-15 21:31:28 -0700

    Added Python solution for the Hacker Rank Practice 'Introduction to conditional statements' problem.

---
+++ Practice/AllDomains/Tutorials/30DaysOfCode/Day3IntroToConditionalStatements.py
@@ -0,0 +1,16 @@
+#/*	author:@shivkrthakur  */
+#!/bin/python3
+
+import sys
+
+N = int(input().strip())
+
+if N % 2 == 0:
+    if N >= 2 and N <=5:
+        print('Not Weird')
+    elif N >= 6 and N <= 20:
+        print('Weird')
+    elif N > 20:
+        print('Not Weird')
+else:
+    print('Weird')
