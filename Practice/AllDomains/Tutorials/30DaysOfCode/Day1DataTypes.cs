3ef4bab45736f1d4d5c3431f5f3e5ee26fb298a8
shiv.kr.thakur <shiv.kr.thakur@hotmail.com>
2017-04-15 21:06:01 -0700

    Added C# solution for the Hacker Rank Practice Data Types problem.

---
+++ Practice/AllDomains/Tutorials/30DaysOfCode/Day1DataTypes.cs
@@ -0,0 +1,18 @@
+/*	author:@shivkrthakur  */
+// Declare second integer, double, and String variables.
+int x;
+double e;
+string t;
+	
+// Read and save an integer, double, and String to your variables.
+x = Convert.ToInt32(Console.ReadLine());
+e = Convert.ToDouble(Console.ReadLine());
+t = Console.ReadLine();
+// Print the sum of both integer variables on a new line.
+Console.WriteLine(x + i);
+// Print the sum of the double variables on a new line.
+Console.WriteLine("{0:N1}", e + d);
+// Concatenate and print the String variables on a new line
+// The 's' variable above should be printed first.
+Console.WriteLine(s + t);
+
