09c2574910b899ce6f4da2fc9904c548a757f001
shiv.kr.thakur <shivkrthakur@users.noreply.github.com>
2017-04-27 20:12:39 -0700

    Changed formating to the C# solution of HackerRank Sorting Algorithm problem 'Quicksort In-Place'.

--- Practice/AllDomains/CoreCS/Algorithms/Sorting/Quicksort1-Partition.cs
+++ Practice/AllDomains/CoreCS/Algorithms/Sorting/Quicksort1-Partition.cs
@@ -6,39 +6,38 @@ class Solution
 	/*	author:@shivkrthakur  */
 	static void partition(int[] N) 
 	{
-			int n = N.Length;
-			int p = N[0];
-			int i = 0, k = 0, j = n -1;
-			int [] O = new int[n];
-			while(++i < n)
+		int n = N.Length;
+		int p = N[0];
+		int i = 0, k = 0, j = n -1;
+		int [] O = new int[n];
+		while(++i < n)
+		{
+			if(N[i] < p)
 			{
-				if(N[i] < p)
-				{
-					O[k++] = N[i];
-				}
-				else if(N[i] > p)
-				{
-					O[j--] = N[i];
-				}
+				O[k++] = N[i];
 			}
-			if(k == j) O[k] = p;
-			else if(k < j) { O[k] = p; O[j] = p; }
+			else if(N[i] > p)
+			{
+				O[j--] = N[i];
+			}
+		}
+		if(k == j) O[k] = p;
+		else if(k < j) { O[k] = p; O[j] = p; }
 
-			//Console.WriteLine($"i:{i} k:{k} j:{j}");
-			Console.WriteLine(string.Join(" ", O));
+		//Console.WriteLine($"i:{i} k:{k} j:{j}");
+		Console.WriteLine(string.Join(" ", O));
 	}
 	/* Tail starts here */
-    static void Main(String[] args) {
-           
-           int _ar_size;
-           _ar_size = Convert.ToInt32(Console.ReadLine());
-           int [] _ar =new int [_ar_size];
-           String elements = Console.ReadLine();
-           String[] split_elements = elements.Split(' ');
-           for(int _ar_i=0; _ar_i < _ar_size; _ar_i++) {
-                  _ar[_ar_i] = Convert.ToInt32(split_elements[_ar_i]); 
-           }
-
-           partition(_ar);
+    static void Main(String[] args) 
+	{
+	   int _ar_size;
+	   _ar_size = Convert.ToInt32(Console.ReadLine());
+	   int [] _ar =new int [_ar_size];
+	   String elements = Console.ReadLine();
+	   String[] split_elements = elements.Split(' ');
+	   for(int _ar_i=0; _ar_i < _ar_size; _ar_i++) {
+			  _ar[_ar_i] = Convert.ToInt32(split_elements[_ar_i]); 
+	   }
+	   partition(_ar);
     }
 }
