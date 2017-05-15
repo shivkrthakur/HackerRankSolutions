98ca4f8b2e5a523b78c58f25f8100a62ca952d05
Shiv Kr Thakur <shivkrthakur@users.noreply.github.com>
2017-05-15 00:59:56 -0700

    Added C# solution for the HackerRank CTCI practice problem 'Bit Manipulation: Lonely Integer'.

---
+++ Practice/AllDomains/Tutorials/CrackingTheCodingInterview/BitManipulationLonelyInteger.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution 
+{
+	/*	author:@shivkrthakur  */
+    static void Main(String[] args) {
+        int n = Convert.ToInt32(Console.ReadLine());
+        string[] a_temp = Console.ReadLine().Split(' ');
+        int[] a = Array.ConvertAll(a_temp,Int32.Parse);
+        int result = 0;
+        a.ToList().ForEach(x => {  result ^= x;});
+        Console.WriteLine(result);
+    }
+}
