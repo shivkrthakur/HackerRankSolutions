3b738c0e16c3d88dd2a88a135fa3972d07d17741
shiv.kr.thakur <shivkrthakur@users.noreply.github.com>
2017-06-03 14:02:23 -0700

    Add JavaScript solution for the HackerRank General programming problem 'Staircase'.

---
+++ Practice/AllDomains/CoreCS/GeneralProgramming/BasicProgramming/Staircase.js
@@ -0,0 +1,36 @@
+/*	author:@shivkrthakur	*/
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+var input_stdin = "";
+var input_stdin_array = "";
+var input_currentline = 0;
+
+process.stdin.on('data', function (data) {
+    input_stdin += data;
+});
+
+process.stdin.on('end', function () {
+    input_stdin_array = input_stdin.split("\n");
+    main();    
+});
+
+function readLine() {
+    return input_stdin_array[input_currentline++];
+}
+
+/////////////// ignore above this line ////////////////////
+
+function main() {
+    var n = parseInt(readLine());
+    for(var i = 1; i <= n; i++)
+    {
+        var str = "";
+        for(var j = n; j > i; j--)
+            str = str + " ";
+        for(var k = 0; k < i; k++)
+            str = str + "#";
+
+        console.log(str);
+    }
+}
