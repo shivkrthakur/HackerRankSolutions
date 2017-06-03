2f590016cc4a8d599940c80447c663ab277f0a05
shiv.kr.thakur <shivkrthakur@users.noreply.github.com>
2017-06-03 13:58:12 -0700

    Add JavaScript solution for the HackerRank General programming problem 'Plus Minus'.

---
+++ Practice/AllDomains/CoreCS/GeneralProgramming/BasicProgramming/PlusMinus.js
@@ -0,0 +1,37 @@
+/*	author:@shivkrthakur	*/
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+var input_stdin = "";
+var input_stdin_array = "";
+var input_currentline = 0;
+
+process.stdin.on('data', function (data) {
+    input_stdin += data;
+});
+
+process.stdin.on('end', function () {
+    input_stdin_array = input_stdin.split("\n");
+    main();    
+});
+
+function readLine() {
+    return input_stdin_array[input_currentline++];
+}
+
+/////////////// ignore above this line ////////////////////
+
+function main() {
+    var n = parseInt(readLine());
+    arr = readLine().split(' ');
+    arr = arr.map(Number);
+    var pC = 0, nC = 0, zC = 0;
+    for(var a = 0; a < n; a++)
+        if(arr[a] == 0) { zC++; }
+        else if(arr[a] > 0) { pC++; }
+        else if(arr[a] < 0) { nC++; }
+
+    console.log(pC/n);
+    console.log(nC/n);
+    console.log(zC/n);
+}
