95e15ded40fec7b6b3fc34707758048588c65db4
shiv.kr.thakur <shivkrthakur@users.noreply.github.com>
2017-06-13 18:30:22 -0700

    Added JavaScript solution for HackerRank General Programming problem 'A Very Big Sum'.

---
+++ Practice/AllDomains/CoreCS/GeneralProgramming/BasicProgramming/AVeryBigSum.js
@@ -0,0 +1,40 @@
+/*	author:@shivkrthakur	*/
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+var input_stdin = "";
+var input_stdin_array = "";
+var input_currentline = 0;
+
+process.stdin.on('data', function (data) {
+    input_stdin += data;
+});
+
+process.stdin.on('end', function () {
+    input_stdin_array = input_stdin.split("\n");
+    main();    
+});
+
+function readLine() {
+    return input_stdin_array[input_currentline++];
+}
+
+/////////////// ignore above this line ////////////////////
+
+function aVeryBigSum(n, ar) {
+    // Complete this function
+    var sum = 0;
+    for(i = 0; i < n; i++){
+        sum += ar[i];
+    }
+    return sum;
+}
+
+function main() {
+    var n = parseInt(readLine());
+    ar = readLine().split(' ');
+    ar = ar.map(Number);
+    var result = aVeryBigSum(n, ar);
+    process.stdout.write("" + result + "\n");
+
+}
