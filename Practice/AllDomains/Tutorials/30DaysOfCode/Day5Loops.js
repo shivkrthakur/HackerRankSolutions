3c8b1290a9dfaba5d8d83cc92146fd0955363079
shiv.kr.thakur <shivkrthakur@users.noreply.github.com>
2017-06-26 20:27:01 -0700

    Added JavaScript solution for HackerRank 30 Days of Code 'Day5: Loops' problem.

---
+++ Practice/AllDomains/Tutorials/30DaysOfCode/Day5Loops.js
@@ -0,0 +1,31 @@
+/*	author:@shivkrthakur  */
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+var input_stdin = "";
+var input_stdin_array = "";
+var input_currentline = 0;
+
+process.stdin.on('data', function (data) {
+    input_stdin += data;
+});
+
+process.stdin.on('end', function () {
+    input_stdin_array = input_stdin.split("\n");
+    main();
+});
+
+function readLine() {
+    return input_stdin_array[input_currentline++];
+}
+
+/////////////// ignore above this line ////////////////////
+
+function main() {
+    var n = parseInt(readLine());
+    for(var i = 1; i <= 10; i++)
+    {
+        var arr = [ n, "x", i, "=", n * i];
+        console.log(arr.join(" "));
+    }
+}
