f83ecfd5fdcbf86cc90571e3c2eac24fd9a443db
shiv.kr.thakur <shivkrthakur@users.noreply.github.com>
2017-06-27 08:55:31 -0700

    Added JavaScript solution for HackerRank 30 Days of Code 'Day7: Arrays'.

---
+++ Practice/AllDomains/Tutorials/30DaysOfCode/Day7Arrays.js
@@ -0,0 +1,34 @@
+/*	author:@shivkrthakur  */
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+var input_stdin = "";
+var input_stdin_array = "";
+var input_currentline = 0;
+
+process.stdin.on('data', function (data) {
+    input_stdin += data;
+});
+
+process.stdin.on('end', function () {
+    input_stdin_array = input_stdin.split("\n");
+    main();
+});
+
+function readLine() {
+    return input_stdin_array[input_currentline++];
+}
+
+/////////////// ignore above this line ////////////////////
+
+function main() {
+    var n = parseInt(readLine());
+    arr = readLine().split(' ');
+    arr = arr.map(Number);
+    var opArr = [];
+    for(var j = n - 1; j >= 0; j--){
+        //console.log(arr[j]);
+        opArr.push(arr[j]);
+    }
+    console.log(opArr.join(" "));
+}
