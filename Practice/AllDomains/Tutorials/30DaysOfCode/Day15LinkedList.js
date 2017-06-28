ce5501e65f29d75a0982da31b2e3d7918d9d447c
shiv.kr.thakur <shivkrthakur@users.noreply.github.com>
2017-06-28 10:41:12 -0700

    Added JavaScript solution for HackerRank 30 Days of Code 'Day15: Linked List'.

---
+++ Practice/AllDomains/Tutorials/30DaysOfCode/Day15LinkedList.js
@@ -0,0 +1,57 @@
+/*	author:@shivkrthakur  */
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+var input_stdin = "";
+var input_stdin_array = "";
+var input_currentline = 0;
+
+process.stdin.on('data', function (data) {
+    input_stdin += data;
+});
+
+process.stdin.on('end', function () {
+    input_stdin_array = input_stdin.split("\n");
+    main();
+});
+function readLine() {
+    return input_stdin_array[input_currentline++];
+}
+function Node(data){
+    this.data=data;
+    this.next=null;
+}
+function Solution(){
+  this.insert=function(head,data){
+          //complete this method
+        var node = new Node(data);
+        if(head){
+            var start = head;
+            while(start.next){
+                start = start.next;
+            }
+            start.next = node;
+        }
+        else head = node;
+        return head;
+    };
+
+  this.display=function(head){
+        var start=head;
+            while(start){
+                process.stdout.write(start.data+" ");
+                start=start.next;
+            }
+    };
+}
+
+function main(){
+    var T=parseInt(readLine());
+    var head=null;
+    var mylist=new Solution();
+    for(i=0;i<T;i++){
+        var data=parseInt(readLine());
+        head=mylist.insert(head,data);
+    }
+    mylist.display(head);
+}
