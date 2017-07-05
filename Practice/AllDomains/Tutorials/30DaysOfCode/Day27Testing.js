c12cbe6c6080b2c369b6f0d79ac3472f09d1af90
shiv.kr.thakur <shivkrthakur@users.noreply.github.com>
2017-07-04 23:14:21 -0700

    Added JavaScript solution for HackerRank 30 Days of Code 'Day27: Testing' problem.

---
+++ Practice/AllDomains/Tutorials/30DaysOfCode/Day27Testing.js
@@ -0,0 +1,12 @@
+/*	author:@shivkrthakur  */
+console.log("5");
+console.log("5 3");
+console.log("-1 90 999 100 0");
+console.log("4 2");
+console.log("0 -1 2 1");
+console.log("3 3");
+console.log("-1 0 1");
+console.log("6 1");
+console.log("-1 0 1 -1 2 3");
+console.log("7 3");
+console.log("-1 0 1 2 3 4 5");
