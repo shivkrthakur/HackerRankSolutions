ec5abb6822f593dbf0f30dc3a686fff711be7588
shiv.kr.thakur <shivkrthakur@users.noreply.github.com>
2017-07-04 23:12:22 -0700

    Added C# solution for HackerRank 30 Days of Code 'Day27: Testing' problem.

---
+++ Practice/AllDomains/Tutorials/30DaysOfCode/Day27Testing.cs
@@ -0,0 +1,20 @@
+/*	author:@shivkrthakur  */
+using System;
+
+public class Solution {
+
+    public static void Main()
+        {
+            Console.WriteLine("5");
+            Console.WriteLine("5 3");
+            Console.WriteLine("-1 90 999 100 0");
+            Console.WriteLine("4 2");
+            Console.WriteLine("0 -1 2 1");
+            Console.WriteLine("3 3");
+            Console.WriteLine("-1 0 1");
+            Console.WriteLine("6 1");
+            Console.WriteLine("-1 0 1 -1 2 3");
+            Console.WriteLine("7 3");
+            Console.WriteLine("-1 0 1 2 3 4 5");
+    }
+}
