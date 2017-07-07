237f4c1f7433a6bdddd017a5a3e99072a3188fd8
shiv.kr.thakur <shivkrthakur@users.noreply.github.com>
2017-07-07 08:48:58 -0700

    Added JavaScript solution for HackerRank CTCI 'Bit Manipulation: Lonely Integer' problem.

---
+++ Practice/AllDomains/Tutorials/CrackingTheCodingInterview/BitManipulationLonelyInteger.js
@@ -0,0 +1,34 @@
+/*	author:@shivkrthakur  */
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+var input_stdin = "";
+var input_stdin_array = "";
+var input_currentline = 0;
+
+process.stdin.on('data', function (data) {
+    input_stdin += data;
+});
+
+process.stdin.on('end', function () {
+    input_stdin_array = input_stdin.split("\n");
+    main();
+});
+
+function readLine() {
+    return input_stdin_array[input_currentline++];
+}
+
+/////////////// ignore above this line ////////////////////
+
+function main() {
+    var n = parseInt(readLine());
+    a = readLine().split(' ');
+    a = a.map(Number);
+
+    var xorSum = 0;
+    for(var i = 0; i < n; i++){
+        xorSum ^= a[i];
+    }
+    console.log(xorSum);
+}
