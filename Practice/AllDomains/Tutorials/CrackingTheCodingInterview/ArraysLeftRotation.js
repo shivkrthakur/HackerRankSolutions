e9a5c8ce84c7e9cf46d2530408dc15fed191d378
shiv.kr.thakur <shivkrthakur@users.noreply.github.com>
2017-07-07 08:48:05 -0700

    Added JavaScript solution for HackerRank CTCI 'Arrays: Left Rotation' problem.

---
+++ Practice/AllDomains/Tutorials/CrackingTheCodingInterview/ArraysLeftRotation.js
@@ -0,0 +1,35 @@
+/*	author:@shivkrthakur  */
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+var input_stdin = "";
+var input_stdin_array = "";
+var input_currentline = 0;
+
+process.stdin.on('data', function (data) {
+    input_stdin += data;
+});
+
+process.stdin.on('end', function () {
+    input_stdin_array = input_stdin.split("\n");
+    main();
+});
+
+function readLine() {
+    return input_stdin_array[input_currentline++];
+}
+
+/////////////// ignore above this line ////////////////////
+function main() {
+    var n_temp = readLine().split(' ');
+    var n = parseInt(n_temp[0]);
+    var k = parseInt(n_temp[1]);
+    a = readLine().split(' ');
+    a = a.map(Number);
+    var output = [];
+    for(var i = 0; k < n && i < n; i++){
+        output.push(a[k]);
+        k = (k + 1) % n;
+    }
+    console.log(output.join(' '));
+}
