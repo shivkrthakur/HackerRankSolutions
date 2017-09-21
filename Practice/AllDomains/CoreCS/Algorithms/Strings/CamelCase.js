2938d36106f226753b624e74c2ea3f4a04cd491c
shiv.kr.thakur <shivkrthakur@users.noreply.github.com>
2017-09-21 15:56:21 -0700

    Added JavaScript solution for HackerRank Strings problem 'Camel Case'.

---
+++ Practice/AllDomains/CoreCS/Algorithms/Strings/CamelCase.js
@@ -0,0 +1,31 @@
+/*	author:@shivkrthakur  */
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+var input_stdin = "";
+var input_stdin_array = "";
+var input_currentline = 0;
+
+process.stdin.on('data', function (data) {
+    input_stdin += data;
+});
+
+process.stdin.on('end', function () {
+    input_stdin_array = input_stdin.split("\n");
+    main();    
+});
+
+function readLine() {
+    return input_stdin_array[input_currentline++];
+}
+
+/////////////// ignore above this line ////////////////////
+
+function main() {
+    var s = readLine();
+    var count = 1;
+    for(var i = 0; i < s.length; i++){
+        if(s[i] == s[i].toUpperCase()) count++;
+    }
+    console.log(count);
+}
