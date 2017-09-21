1a2ebd2973ca971bdd89fc829f1cd1627cec77dd
shiv.kr.thakur <shivkrthakur@users.noreply.github.com>
2017-09-21 15:55:46 -0700

    Added C# solution for HackerRank Strings problem 'Camel Case'.

---
+++ Practice/AllDomains/CoreCS/Algorithms/Strings/CamelCase.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution {
+    static void Main(String[] args) 
+	{
+        string s = Console.ReadLine();
+        int wordCount = s.Count(x => char.IsUpper(x)) + 1;
+        /*int wordCount = 1;
+        for(int  i = 0; i < s.Length; i++)
+        {
+          if(char.IsUpper(s[i])) wordCount++;
+        }
+        */
+        Console.WriteLine(wordCount);
+    }
+}
