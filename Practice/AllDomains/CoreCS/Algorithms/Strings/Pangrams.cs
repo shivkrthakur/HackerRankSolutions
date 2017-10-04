3d97c909e25dca721cd73e1bd49342d986b023eb
shiv.kr.thakur <shivkrthakur@users.noreply.github.com>
2017-10-03 18:09:15 -0700

    Added C# solution for HackerRank Strings problem 'Pangrams'.

---
+++ Practice/AllDomains/CoreCS/Algorithms/Strings/Pangrams.cs
@@ -0,0 +1,13 @@
+/*	author:@shivkrthakur  */
+using System;
+using System.Collections.Generic;
+using System.IO;
+class Solution {
+    static void Main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution */
+        string input = Console.ReadLine();
+        HashSet<char> hashSet = new HashSet<char>(input.ToLower().ToCharArray());
+		if(hashSet.Contains(' ') && hashSet.Count == 27) Console.WriteLine("pangram");
+        else Console.WriteLine("not pangram");
+    }
+}
