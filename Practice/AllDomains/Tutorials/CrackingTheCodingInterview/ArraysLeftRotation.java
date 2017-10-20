164c783ce8e89b85940af79f76d43dda30df016a
shiv.kr.thakur <shivkrthakur@users.noreply.github.com>
2017-10-20 15:38:23 -0700

    Added Java solution for HackerRank Cracking the Coding Interview Tries problem 'Arrays: Left Rotation'.

---
+++ Practice/AllDomains/Tutorials/CrackingTheCodingInterview/ArraysLeftRotation.java
@@ -0,0 +1,37 @@
+/*	author:@shivkrthakur  */
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static int[] arrayLeftRotation(int[] a, int n, int k) {
+      int[] b = new int[n];
+      for(int x = 0; x < n; x++)
+      {
+          int index = (n + k + x) % n;
+          b[x] = a[index];
+      }
+        return b;
+    }
+    
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int k = in.nextInt();
+        int a[] = new int[n];
+        for(int a_i=0; a_i < n; a_i++){
+            a[a_i] = in.nextInt();
+        }
+      
+        int[] output = new int[n];
+        output = arrayLeftRotation(a, n, k);
+        for(int i = 0; i < n; i++)
+            System.out.print(output[i] + " ");
+      
+        System.out.println();
+      
+    }
+}
