90a5d264702f796e615dd63916bc78dd5a150117
shiv.kr.thakur <shivkrthakur@users.noreply.github.com>
2017-11-03 23:49:18 -0700

    Modified C# solution for the HackerRank String algorithm problem 'Bear and Steady Gene'.

--- Practice/AllDomains/CoreCS/Algorithms/Strings/BearAndSteadyGene.cs
+++ Practice/AllDomains/CoreCS/Algorithms/Strings/BearAndSteadyGene.cs
@@ -1,9 +1,9 @@
+/*	author:@shivkrthakur  */
 using System;
 using System.Collections.Generic;
 using System.IO;
 class Solution 
 {
-    /*	author:@shivkrthakur  */
     static void Main(String[] args) 
 	{
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution */
