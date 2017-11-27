c0ad990457795bc9931150bdd23cf4ae27afb59b
Shiv Kr Thakur <shivkrthakur@users.noreply.github.com>
2017-11-26 20:33:00 -0800

    Added JavaScript solution for the HackerRank '10 Days of JavaScript' programming problem 'Day 2: Conditional Statements If Else'.

---
+++ Practice/AllDomains/Tutorials/10DaysOfJavaScript/Day2ConditionalStatementsIfElse.js
@@ -0,0 +1,41 @@
+/*	author:@shivkrthakur  */
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function getGrade(score) {
+    let grade;
+    // Write your code here
+    if(25 < score && score <= 30) grade = "A";
+    else if(20 < score && score <= 25) grade = "B";
+    else if(15 < score && score <= 20) grade = "C";
+    else if(10 < score && score <= 15) grade = "D";
+    else if(5 < score && score <= 10) grade = "E";
+    else if(0 <= score && score <= 5) grade = "F";
+    return grade;
+}
+
+function main() {
+    const score = +(readLine());
+    console.log(getGrade(score));
+}
