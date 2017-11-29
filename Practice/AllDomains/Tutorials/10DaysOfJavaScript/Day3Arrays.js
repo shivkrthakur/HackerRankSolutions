3c99fee8c5456049aa03eb08b88b308d8eb09373
Shiv Kr Thakur <shivkrthakur@users.noreply.github.com>
2017-11-28 21:59:11 -0800

    Added JavaScript solution for the HackerRank '10 Days of JavaScript' programming problem 'Day 3: Arrays'.

---
+++ Practice/AllDomains/Tutorials/10DaysOfJavaScript/Day3Arrays.js
@@ -0,0 +1,46 @@
+/*	author:@shivkrthakur  */
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/**
+*   Return the second largest number in the array.
+*   @param {Number[]} nums - An array of numbers.
+*   @return {Number} The second largest number in the array.
+**/
+function getSecondLargest(nums) {
+    // Complete the function
+    nums = nums.sort(function(a,b){
+        return parseInt(a) - parseInt(b);
+    });
+    var max = nums[nums.length - 1];
+    for(var i = nums.length - 2; i >= 0; i--)
+        if(max != nums[i]) return nums[i];
+}
+
+function main() {
+    const n = +(readLine());
+    const nums = readLine().split(' ').map(Number);
+    
+    console.log(getSecondLargest(nums));
+}
