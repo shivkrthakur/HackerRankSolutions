19263b82bfd52d04afb7a331961f5ec762dbd9bf
Shiv Kr Thakur <shivkrthakur@users.noreply.github.com>
2017-11-28 21:58:21 -0800

    Added JavaScript solution for the HackerRank '10 Days of JavaScript' programming problem 'Day 2: Loops'.

---
+++ Practice/AllDomains/Tutorials/10DaysOfJavaScript/Day2Loops.js
@@ -0,0 +1,45 @@
+/*	author:@shivkrthakur  */
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the vowelsAndConsonants function.
+ * Print your output using 'console.log()'.
+ */
+function vowelsAndConsonants(s) {
+    for(var i = 0; i < s.length; i++){
+        if(s[i] == 'a' | s[i] == 'e' | s[i] == 'i' | s[i] == 'o' | s[i] == 'u')
+            console.log(s[i]);
+    }
+    
+    for(var i = 0; i < s.length; i++){
+        if(s[i] != 'a' & s[i] != 'e' & s[i] != 'i' & s[i] != 'o' & s[i] != 'u')
+            console.log(s[i]);
+    }
+}
+
+function main() {
+    const s = readLine();
+    vowelsAndConsonants(s);
+}
