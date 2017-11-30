1c07a42d7a6b0ced0b9469d5e0e2ed130e7a666d
Shiv Kr Thakur <shivkrthakur@users.noreply.github.com>
2017-11-30 15:41:58 -0800

    Added JavaScript solution for the HackerRank '10 Days of JavaScript' programming problem 'Day 5: Arrow Functions'.

---
+++ Practice/AllDomains/Tutorials/10DaysOfJavaScript/Day5ArrowFunctions.js
@@ -0,0 +1,49 @@
+/*	author:@shivkrthakur  */
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Modify and return the array so that all even elements are doubled and all odd elements are tripled.
+ * 
+ * Parameter(s):
+ * nums: An array of numbers.
+ */
+function modifyArray(nums) {
+    var output = [];
+    nums.forEach(function(x){
+        if((x % 2) == 0){
+            x = x * 2;
+        }
+        else x = x * 3;
+        output.push(x);
+    });
+    return output;
+}
+
+function main() {
+    const n = +(readLine());
+    const a = readLine().split(' ').map(Number);
+    
+    console.log(modifyArray(a).toString().split(',').join(' '));
+}
