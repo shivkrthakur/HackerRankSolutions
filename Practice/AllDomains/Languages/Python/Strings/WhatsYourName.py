07b518223f2fe15d01f82308cb5e82356ba0390b
Shiv Kr Thakur <shivkrthakur@users.noreply.github.com>
2017-12-10 17:55:12 -0800

    Added Python solution for HackerRank Python Strings problem 'What''s your name'.

---
+++ Practice/AllDomains/Languages/Python/Strings/WhatsYourName.py
@@ -0,0 +1,3 @@
+#/*	author:@shivkrthakur  */
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+print "Hello", raw_input().strip(), raw_input().strip() + "! You just delved into python."
