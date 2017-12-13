95d0f38f31649b73c600b191cce55831b2961caf
Shiv Kr Thakur <shivkrthakur@users.noreply.github.com>
2017-12-12 19:14:55 -0800

    Added Python solution for HackerRank Python Strings problem 'Text Wrap'.

---
+++ Practice/AllDomains/Languages/Python/Strings/TextWrap.py
@@ -0,0 +1,6 @@
+#/*	author:@shivkrthakur  */
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+import textwrap
+#inputString = raw_input()
+#inputInt = int(raw_input())
+print(textwrap.fill(raw_input(),int(raw_input())))
