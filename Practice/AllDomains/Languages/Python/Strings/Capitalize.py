1a6ebe91b8d9924f2e6bf59fe34e5e99f5cc651c
Shiv Kr Thakur <shivkrthakur@users.noreply.github.com>
2017-12-13 20:36:25 -0800

    Added Python solution for HackerRank Python Strings problem 'Capitalize'.

---
+++ Practice/AllDomains/Languages/Python/Strings/Capitalize.py
@@ -0,0 +1,5 @@
+#/*	author:@shivkrthakur  */
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+input = raw_input().strip()
+print ' '.join([x.capitalize() for x in input.split(' ')])
+
