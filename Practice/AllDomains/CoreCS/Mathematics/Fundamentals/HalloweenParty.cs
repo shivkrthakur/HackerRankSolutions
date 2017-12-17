e1b33066df0b1344f32c0a45cc39ceb3352b6a0a
Shiv Kr Thakur <shivkrthakur@users.noreply.github.com>
2017-12-16 22:28:22 -0800

    Added C# solution for HackerRank Fundamental math problem 'Halloween Party'.

---
+++ Practice/AllDomains/CoreCS/Mathematics/Fundamentals/HalloweenParty.cs
@@ -0,0 +1,17 @@
+#/*	author:@shivkrthakur  */
+using System;
+using System.Collections.Generic;
+using System.IO;
+class Solution {
+    static void Main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution */
+        int t = Convert.ToInt32(Console.ReadLine().Trim());
+        for(int i = 0; i < t; i++){
+            long n = Convert.ToInt64(Console.ReadLine().Trim());
+            long h = n/2;
+            long j = n - h;
+            long o = h * j;
+            Console.WriteLine(o);
+        }
+    }
+}
